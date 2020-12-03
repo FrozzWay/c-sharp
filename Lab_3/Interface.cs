8a561c76eb9df29871113be9b60d234c70449d1e
Aleksandr Okunev <frozz.way@mail.ru>
2020-12-03 23:08:10 +0500

    Lab_3 1.0.0

---
+++ Lab_3/Interface.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_3
+{
+    public class Interface
+    {
+        const string fileName = @"c:\Temp\organization.xml";
+
+
+        public static void Menu()
+        {
+            Console.WriteLine("Action list:\n 1. Add employee\n 2. View employees\n 3. [Task2]\n 4. [Task3]\n 5. Write XML\n 6. Read XML");
+            switch (Console.ReadLine())
+            {
+                case "1": Add(); break;
+                case "2": View_all(); break;
+                case "3": Task2(); break;
+                case "4": Task3(); break;
+                case "5": Write(); break;
+                case "6": Read(); break;
+                default: return;
+            }
+        }
+
+        // Добавление работника
+        static void Add()
+        {
+            Console.WriteLine("\n #### ADDING MENU ####");
+            Console.WriteLine("\nAdd:\n 1. HourGuy\n 2. FixedGuy");
+            string x = Console.ReadLine();
+            Console.WriteLine("\n Input name"); string _name = Console.ReadLine();
+            Console.WriteLine("\n Input birthday"); string _birthday = Console.ReadLine();
+            Console.WriteLine("\n Input position"); string _position = __position();
+            switch (x)
+            {
+                case "1":
+                    Console.WriteLine("\n Input hour stake"); double _hour_stake = Convert.ToDouble((Console.ReadLine()));
+                    Program.organization.employees.Add(new Program.HourGuy(_name, _birthday, _position, "HourGuy", _hour_stake));
+                    Program.organization.employees.Last().view();
+                    Console.WriteLine($"\n Average salary of this organization = {Program.organization.getAverageSalary()}");
+                    Menu();
+                    break;
+                case "2":
+                    Console.WriteLine("\n Input fixed stake"); double _fixed_stake = Convert.ToDouble((Console.ReadLine()));
+                    Program.organization.employees.Add(new Program.FixedGuy(_name, _birthday, _position, "FixedGuy", _fixed_stake));
+                    Program.organization.employees.Last().view();
+                    Console.WriteLine($"\n Average salary of this organization = {Program.organization.getAverageSalary()}");
+                    Menu();
+                    break;
+                default:
+                    Menu(); return;
+            }
+        }
+  
+        static string __position()
+        {
+            Console.WriteLine("\nChoose position:\n 1. Boss\n 2. NotBoss");
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    return "Boss";
+                case "2":
+                    return "NotBoss";
+                default:
+                    return __position();
+            }
+        }
+
+
+        // Упорядочить последовательность
+        static void View_all()
+        {
+            Sort();
+            foreach (Program.Employee obj in Program.organization.employees) obj.view();
+            Console.WriteLine($"\nAverage organization salary - {Program.organization.getAverageSalary()}\n");
+            Menu();
+        }
+
+
+        static void Sort()
+        {
+            Program.organization.employees.Sort(delegate (Program.Employee x, Program.Employee y)
+            {
+                if (x.salary == y.salary)
+                    return x.name.CompareTo(y.name);
+                return y.salary.CompareTo(x.salary);
+            });
+        }
+
+
+        static void Task2()
+        {
+            Sort(); int i = 0;
+            try
+            {
+                Console.WriteLine("\n");
+                while (i < 5)
+                {
+                    Console.WriteLine(Program.organization.employees[i].name);
+                    i++;
+                }
+                Console.WriteLine("\n");
+            }
+            catch (Exception ex) { Console.WriteLine("\nLess than 5 employees!\n"); }
+            Menu();
+        }
+
+        static void Task3()
+        {
+            Sort();
+            try
+            {
+                int len = Program.organization.employees.Count;
+                Console.WriteLine($"\n{ Program.organization.employees[len - 1]._id}, { Program.organization.employees[len-2]._id}, { Program.organization.employees[len-3]._id}\n");
+            }
+            catch (Exception ex) { Console.WriteLine("\nLess than 3 employees!\n"); }
+            Menu();
+        }
+
+
+
+
+        // Записать XML
+        static void Write()
+        { 
+            Program.organization.ToXML(fileName);
+            Console.WriteLine("\n File has been written!\n");
+            Menu();
+        }
+
+        // Считать XML
+        static void Read()
+        {
+            try
+            {
+                Program.organization = Program.Organization.FromXml(fileName);
+                Console.WriteLine("\nFile has been read!\n");
+            }
+            catch (Exception ex) { Console.WriteLine($"\nError: {ex.Message}\n"); }
+            Menu();
+        }
+    }
+}
--- Lab_3/Program.cs
+++ Lab_3/Program.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Linq;
 
 namespace Lab_3
 {
-    class Program
+    public class Program
     {
 
         public class Organization
         {
-            public double TotalSallary = 0;
-            public double AmountOfEmployees = 0;
-            public double getAverageSallary(double addSallary) {
-                return TotalSallary / AmountOfEmployees;
+            public double getAverageSalary() {
+                double sum = 0;
+                if (employees.Count != 0)
+                    foreach (Employee obj in employees)
+                        sum += obj.salary;
+                return sum / employees.Count;
             }
-            public static List<Employee> employees = new List<Employee>();
+            public List<Employee> employees = new List<Employee>();
 
             public Organization() { }
+
+            public void ToXML(string fileName)
+            {
+                using (var stream = new FileStream(fileName, FileMode.Create))
+                {
+                    XmlSerializer XML = new XmlSerializer(typeof(List<Employee>));
+                    XML.Serialize(stream, employees);
+                    stream.Flush();
+                }
+            }
+
+            public static Organization FromXml(string fileName)
+            {
+                var organization = new Organization();
+                using (var stream = File.OpenRead(fileName))
+                {
+                    XmlSerializer XML = new XmlSerializer(typeof(List<Employee>));
+                    var employees = XML.Deserialize(stream) as IEnumerable<Employee>;
+                    if (employees != null) organization.employees.AddRange(employees);
+                }
+                return organization;
+            }
         };
 
 
@@ -28,6 +56,8 @@ namespace Lab_3
 
 
         // Абстрактный класс Сотрудник
+        [XmlInclude(typeof(HourGuy))]
+        [XmlInclude(typeof(FixedGuy))]
         public abstract class Employee
         {
             static int num = 0;
@@ -37,8 +67,9 @@ namespace Lab_3
             public string position;
             public double bonus;
             public double salary;
+            public string type;
 
-            public Employee(string _name, string _birthday, string _position)
+            public Employee(string _name, string _birthday, string _position, string _type)
             {
                 _id = num;
                 num++;
@@ -46,10 +77,17 @@ namespace Lab_3
                 birthday = _birthday;
                 position = _position;
                 bonus = Position[position];
-                salary = countSallary();
+                type = _type;
             }
 
-            public abstract double countSallary();
+            public Employee() { }
+
+            public abstract double countSalary();
+
+            public void view()
+            {
+                Console.WriteLine($"\nEmployee № {_id}\nName - {name}\nBirthday - {birthday}\nPosition - {position}\nBonus - {bonus}\nType - {type}\nSalary - {salary}\n");
+            }
 
         }
 
@@ -58,16 +96,19 @@ namespace Lab_3
         public class HourGuy : Employee
         {
             public double hour_stake;
-            public override double countSallary()
+            public override double countSalary()
             {
                 return 20.8 * 8 * hour_stake + bonus;
             }
             
-            public HourGuy(string _name, string _birthday, string _position, double _hour_stake)
-                : base(_name, _birthday, _position)
+            public HourGuy(string _name, string _birthday, string _position, string _type, double _hour_stake)
+                : base(_name, _birthday, _position, _type)
             {
                 hour_stake = _hour_stake;
+                salary = countSalary();
             }
+
+            public HourGuy() { }
         }
 
 
@@ -75,21 +116,37 @@ namespace Lab_3
         public class FixedGuy : Employee
         {
             public double fixed_stake;
-            public override double countSallary()
+            public override double countSalary()
             {
                 return fixed_stake + bonus;
             }
 
-            public FixedGuy(string _name, string _birthday, string _position, double _fixed_stake)
-                : base(_name, _birthday, _position)
+            public FixedGuy(string _name, string _birthday, string _position, string _type, double _fixed_stake)
+                : base(_name, _birthday, _position, _type)
             {
                 fixed_stake = _fixed_stake;
+                salary = countSalary();
             }
+
+            public FixedGuy() { }
         }
+
+        public static Organization organization = new Organization();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Debug();
+            Interface.Menu();
+        }
+
+        static void Debug()
+        {
+            organization.employees.Add(new HourGuy("Alice", "bd", "NotBoss", "HourGuy", 50));
+            organization.employees.Add(new HourGuy("Bob", "bd", "NotBoss", "HourGuy", 50));
+            organization.employees.Add(new HourGuy("Debra", "bd", "Boss", "HourGuy", 50));
+            organization.employees.Add(new FixedGuy("Oleg", "bd", "NotBoss", "FixedGuy", 5000));
+            organization.employees.Add(new FixedGuy("Julia", "bd", "NotBoss", "FixedGuy", 9000));
+            organization.employees.Add(new FixedGuy("Mark", "bd", "Boss", "FixedGuy", 10000));
         }
     }
 }
